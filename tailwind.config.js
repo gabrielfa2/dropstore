4b1185f3e93fc698cc2cae314bc6d9a7f35cc48c
Gabriel <eugabrielfa07@gmail.com>
2025-08-27 14:05:48 -0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {
       // Adicionando sua cor personalizada
       colors: {
-        'cream': '#ffffff',
+        'cream': '#fcf0e4',
       },
       // Mantendo a animação existente
       animation: {
