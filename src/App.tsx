b8b72ff09dd2e026f61af90b0eadb1d45e4ca7de
Gabriel <eugabrielfa07@gmail.com>
2025-08-27 13:57:30 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-stone-100">
+    <div className="min-h-screen bg-stone-300">
       <Header />
       <HeroSection />
       <BrandCarousel />
