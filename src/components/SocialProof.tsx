4a7c18d75800403d38ae18b4c06d9028d0d72067
Gabriel <eugabrielfa07@gmail.com>
2025-08-27 15:26:38 -0300

    Updated SocialProof.tsx

--- src/components/SocialProof.tsx
+++ src/components/SocialProof.tsx
@@ -79,7 +79,7 @@ const SocialProof = () => {
           <div>
             <><img src="public/fototabelaprototipo.PNG"></img> </>
             <><img src="/foto3.png"></img> </>
-            <div className="text-gray-600 font-medium">Colocar aquela tabela de dúvidas frequetnes com as principais duvidas e um botão para a pessoa enviar duvida pelo whatsapp que com uma descrição de resposta 24/7 (que aí responde o wpp por IA, as duvidas, tirada de pedido, etc, com a IA dizendo que também faz o pedido)      o codigo do antes e depois vai ficar entre as camisas e o botão de ver todos os produtos</div>
+            <div className="text-gray-600 font-medium">Colocar aquela tabela de dúvidas frequetnes com as principais duvidas e um botão para a pessoa enviar duvida pelo whatsapp que com uma descrição de resposta 24/7 (que aí responde o wpp por IA, as duvidas, tirada de pedido, etc, com a IA dizendo que também faz o pedido)</div>
           </div>
         </div>
       </div>
