8bbc0f6792d19b2c3804c72b105da1c5d04033fe
Gabriel <eugabrielfa07@gmail.com>
2025-08-27 13:54:46 -0300

    Updated SocialProof.tsx

--- src/components/SocialProof.tsx
+++ src/components/SocialProof.tsx
@@ -27,7 +27,7 @@ const SocialProof = () => {
   ];
 
   return (
-    <section className="bg-white py-16">
+    <section className="py-16">
       <div className="max-w-7xl mx-auto px-4">
         {/* Header */}
         <div className="text-center mb-12">
