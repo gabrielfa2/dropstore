5988b7289dab8755f995c13b2fdb008690df84d8
Gabriel <eugabrielfa07@gmail.com>
2025-09-01 19:08:25 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione esta linha!
+  // Se o seu repositório se chama 'dropstore', por exemplo:
+  base: '/dropstore/', 
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
