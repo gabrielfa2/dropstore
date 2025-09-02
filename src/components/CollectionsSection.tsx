2ac7a408bba7424a872ca87352b755e428176a86
Gabriel <eugabrielfa07@gmail.com>
2025-09-02 13:15:00 -0300

    Updated CollectionsSection.tsx

--- src/components/CollectionsSection.tsx
+++ src/components/CollectionsSection.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 
-// Dados das coleções com classes de altura específicas para desktop
+// Dados das coleções com classes de LARGURA para desktop
 const collections = [
   {
     id: 1,
     name: 'Oversized',
     href: '#oversized',
     imageSrc: 'https://images.pexels.com/photos/1484806/pexels-photo-1484806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    mobileClasses: 'col-span-2 h-64',
-    desktopHeight: 'md:h-96', // <-- Card alto
+    // Mobile: ocupa 2 colunas. Desktop: ocupa 1 de 3 colunas (mais estreito)
+    gridClasses: 'col-span-2 md:col-span-1',
   },
   {
     id: 2,
     name: 'Polo',
     href: '#polo',
     imageSrc: 'https://images.pexels.com/photos/3765171/pexels-photo-3765171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    mobileClasses: 'col-span-1 h-64',
-    desktopHeight: 'md:h-80', // <-- Card baixo
+    // Mobile: ocupa 1 coluna. Desktop: ocupa 2 de 3 colunas (mais largo)
+    gridClasses: 'col-span-1 md:col-span-2',
   },
   {
     id: 3,
     name: 'Shorts',
     href: '#shorts',
     imageSrc: 'https://images.pexels.com/photos/4066290/pexels-photo-4066290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    mobileClasses: 'col-span-1 h-64',
-    desktopHeight: 'md:h-80', // <-- Card baixo
+    // Mobile: ocupa 1 coluna. Desktop: ocupa 2 de 3 colunas (mais largo)
+    gridClasses: 'col-span-1 md:col-span-2',
   },
   {
     id: 4,
     name: 'Tênis',
     href: '#tenis',
     imageSrc: 'https://images.pexels.com/photos/267202/pexels-photo-267202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    mobileClasses: 'col-span-2 h-64',
-    desktopHeight: 'md:h-96', // <-- Card alto
+    // Mobile: ocupa 2 colunas. Desktop: ocupa 1 de 3 colunas (mais estreito)
+    gridClasses: 'col-span-2 md:col-span-1',
   },
 ];
 
@@ -50,14 +50,14 @@ const CollectionsSection = () => {
           </p>
         </div>
 
-        {/* Grid das Coleções */}
-        <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+        {/* Grid das Coleções - Alterado para 3 colunas no desktop */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {collections.map((collection) => (
             <a
               key={collection.id}
               href={collection.href}
-              // Classes para layout mobile e desktop são aplicadas aqui
-              className={`${collection.mobileClasses} ${collection.desktopHeight} md:col-span-1 relative rounded-2xl overflow-hidden group shadow-lg`}
+              // Adicionada altura fixa para todos os cards
+              className={`${collection.gridClasses} h-80 relative rounded-2xl overflow-hidden group shadow-lg`}
             >
               {/* Imagem de Fundo */}
               <img
