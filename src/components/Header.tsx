7db47ce013f35af9e9823c9aec563c41613901eb
Gabriel <eugabrielfa07@gmail.com>
2025-09-02 13:54:54 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
           <div className="flex items-center justify-start w-1/3 space-x-2">
             {/* Ícone da Lista (Menu Hambúrguer) - SÓ APARECE NO MOBILE */}
             <button className="md:hidden p-2">
-              <Menu className="w-6 h-6" />
+              <Menu className="w-12 h-12" />
             </button>
           </div>
 
