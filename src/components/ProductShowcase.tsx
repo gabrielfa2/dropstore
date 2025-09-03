14cb79fdfd6ad93ebfce3d36eeb037d32f00dd9d
Gabriel <eugabrielfa07@gmail.com>
2025-09-03 11:20:38 -0300

    Updated ProductShowcase.tsx

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -48,7 +48,7 @@ const ProductShowcase = () => {
   ];
 
   return (
-    <section className="pt-8 pb-0">
+    <section className="pt-8 pb-16">
       <div className="max-w-7xl mx-auto px-4">
         {/* Header */}
         <div className="text-center mb-12">
