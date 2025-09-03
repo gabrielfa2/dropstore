6ff93b4e8fffa1c930cc2c5eb9159019374da62e
Gabriel <eugabrielfa07@gmail.com>
2025-09-03 13:04:15 -0300

    Updated LimitedOffers.tsx

--- src/components/LimitedOffers.tsx
+++ src/components/LimitedOffers.tsx
@@ -35,7 +35,7 @@ const LimitedOffers = () => {
   const afterImage = "/foto3.PNG";
 
   return (
-    <section className="py-16">
+    <section className="py-2">
       <div className="max-w-7xl mx-auto px-4">
 
         {/* Header da Seção */}
