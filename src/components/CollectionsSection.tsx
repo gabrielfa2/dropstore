53b48e2c0366ba87b0ce7f94f162b8e85dc3bb9e
Gabriel <eugabrielfa07@gmail.com>
2025-09-03 15:28:21 -0300

    Updated CollectionsSection.tsx

--- src/components/CollectionsSection.tsx
+++ src/components/CollectionsSection.tsx
@@ -98,7 +98,7 @@ const CollectionCard = ({ collection }) => {
 
 const CollectionsSection = () => {
   return (
-    <section className="py-6">
+    <section className="py-5">
       <div className="max-w-7xl mx-auto px-4">
         <div className="text-center mb-12">
           <h3 className="text-4xl font-black text-black font-display">COLEÇÕES</h3>
