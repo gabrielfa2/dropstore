3cd8bc921205e1637dbe57577a93a234b686ac34
Gabriel <eugabrielfa07@gmail.com>
2025-09-06 19:14:07 -0300

    Updated ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -201,7 +201,7 @@ const ProductsPage = () => {
       </div>
       <div className="mt-4">
         <p className="text-xs md:text-sm text-gray-500 uppercase tracking-wider">{product.brand}</p>
-        <h4 className="text-md md:text-lg font-bold text-black truncate">
+        <h4 className="text-md md:text-lg font-bold text-black truncate font-price">
           {product.name}
         </h4>
         <div className="mt-0 flex items-baseline gap-2">
