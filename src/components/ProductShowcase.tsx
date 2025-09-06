5da2216f58c088a7a7b93b5f42d04622f41f2835
Gabriel <eugabrielfa07@gmail.com>
2025-09-06 15:47:28 -0300

    Updated ProductShowcase.tsx

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -100,7 +100,7 @@ const ProductShowcase = () => {
              <span className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-25 rounded-lg transform translate-y-0.5 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:translate-y-1 group-hover:duration-[250ms] group-active:translate-y-px" />
              <span className="absolute top-0 left-0 w-full h-full rounded-lg bg-gradient-to-l from-[hsl(217,33%,16%)] via-[hsl(217,33%,32%)] to-[hsl(217,33%,16%)]" />
              <div className="relative flex items-center justify-between py-3 px-6 text-lg text-white rounded-lg transform -translate-y-1 bg-gradient-to-r from-[#f27121] via-[#e94057] to-[#8a2387] gap-3 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:-translate-y-1.5 group-hover:duration-[250ms] group-active:-translate-y-0.5 brightness-100 group-hover:brightness-110">
-               <span className="select-none font-bold">Ver Produto Detalhado 🔥</span>
+               <span className="select-none font-bold">Quero conhecer 🔥</span>
              </div>
            </Link>
         </div>
