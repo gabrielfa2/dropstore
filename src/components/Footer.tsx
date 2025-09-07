55888f525fcdd94e9e5fd7a96de9271651dd7967
Gabriel <eugabrielfa07@gmail.com>
2025-09-07 16:45:01 -0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,10 @@ import FooterDivider from './FooterDivider'; // 1. Importe o novo componente
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white pt-15"> {/* Removido o padding (py-12) para o divisor ficar no topo */}
+    <footer className="bg-black text-white pt-8"> {/* Removido o padding (py-12) para o divisor ficar no topo */}
       <div className="max-w-7xl mx-auto px-4">
 
+        
         {/* 2. Adicione o divisor animado aqui */}
         <div className="mb-12"> {/* Este container cria o espaço entre a linha e o conteúdo */}
           <FooterDivider />
