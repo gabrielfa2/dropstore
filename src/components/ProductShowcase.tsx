9859153ee633550b6390c5dab7857fef0a68e48e
Gabriel <eugabrielfa07@gmail.com>
2025-09-07 13:22:59 -0300

    Updated ProductShowcase.tsx

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -96,7 +96,7 @@ const ProductShowcase = () => {
 
         {/* Call to Action com o novo botão */}
         <div className="text-center mt-12">
-           <Link to="produtos" className="relative group border-none bg-transparent p-0 outline-none cursor-pointer font-mono font-light uppercase text-base inline-block">
+           <Link to="/produtos" className="relative group border-none bg-transparent p-0 outline-none cursor-pointer font-mono font-light uppercase text-base inline-block">
              <span className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-25 rounded-lg transform translate-y-0.5 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:translate-y-1 group-hover:duration-[250ms] group-active:translate-y-px" />
              <span className="absolute top-0 left-0 w-full h-full rounded-lg bg-gradient-to-l from-[hsl(217,33%,16%)] via-[hsl(217,33%,32%)] to-[hsl(217,33%,16%)]" />
              <div className="relative flex items-center justify-between py-3 px-6 text-lg text-white rounded-lg transform -translate-y-1 bg-gradient-to-r from-[#f27121] via-[#e94057] to-[#8a2387] gap-3 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:-translate-y-1.5 group-hover:duration-[250ms] group-active:-translate-y-0.5 brightness-100 group-hover:brightness-110">
