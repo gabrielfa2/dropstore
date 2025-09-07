1135cfa1367716e4563af7ee11dea59b0dcad208
Gabriel <eugabrielfa07@gmail.com>
2025-09-07 16:08:54 -0300

    Updated CollectionPage.tsx

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -405,7 +405,7 @@ const CollectionPage = () => {
             <h1 className="text-4xl md:text-6xl font-black mb-2 font-display">
               {config.title}
             </h1>
-            <p className="text-xl md:text-2xl font-price">
+            <p className="text-4xl font-black text-black mb-1 font-display">
               {config.subtitle}
             </p>
           </div>
