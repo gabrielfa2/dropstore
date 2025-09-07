dbe09dd9d1087ad9122019a88cc4278fdafdcd1c
Gabriel <eugabrielfa07@gmail.com>
2025-09-07 17:09:59 -0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ export default {
       },
       animation: {
         // Substituído 'scroll' por 'marquee'
-        marquee: 'marquee 10s linear infinite', 
+        marquee: 'marquee 40s linear infinite', 
         'pulse-glow': 'pulse-glow 6s ease-in-out infinite',
       },
       keyframes: {
