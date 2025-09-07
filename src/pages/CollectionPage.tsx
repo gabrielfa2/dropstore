a991fec34e654fa78fc9da703cff2ec77b805661
Gabriel <eugabrielfa07@gmail.com>
2025-09-07 14:43:46 -0300

    Updated CollectionPage.tsx

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -361,7 +361,7 @@ const CollectionPage = () => {
       </div>
 
       {/* Hero Section */}
-      <div className="relative h-32 md:h-64 mb-12 overflow-hidden">
+      <div className="relative h-38 md:h-64 mb-12 overflow-hidden">
         <img
           src={config.hero}
           alt={config.title}
