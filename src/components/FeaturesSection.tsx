f96998cfa471ddc40b362a159d468e90b24b135f
Gabriel <eugabrielfa07@gmail.com>
2025-09-10 17:32:26 -0300

    Updated FeaturesSection.tsx

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -104,7 +104,7 @@ const FeaturesSection = () => {
                   <h4 className="text-lg font-bold text-black mb-1">
                     {feature.title}
                   </h4>
-                  <p className="text-gray text-sm">
+                  <p className="text-black-200 text-sm">
                     {feature.description}
                   </p>
                 </div>
