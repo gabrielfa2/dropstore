e77ab09158aafd3c1946fcd4f6b2bc0e42a8154a
Gabriel <eugabrielfa07@gmail.com>
2025-09-10 17:02:17 -0300

    Updated ImageSliderSection.tsx

--- src/components/ImageSliderSection.tsx
+++ src/components/ImageSliderSection.tsx
@@ -27,7 +27,7 @@ const ImageSliderSection = () => {
         </p>
 
         {/* Slider destacado */}
-        <div className="w-full rounded-2xl overflow-hidden bg-black/50 backdrop-blur-md p-2 shadow-xl">
+        <div className="w-full rounded-2xl overflow-hidden bg-black/30 backdrop-blur-md p-2 shadow-xl">
           <ImgComparisonSlider 
             value="30" 
             style={{ 
