612621531a6742c988e57d6a4fcbc637b7c741b7
Gabriel <eugabrielfa07@gmail.com>
2025-09-10 12:28:38 -0300

    Updated FeaturesSection.tsx

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -65,7 +65,6 @@ const FeaturesSection = () => {
       <div className="absolute top-0 left-0 w-full h-full z-0">
         <video
           autoPlay
-          loop
           muted
           playsInline
           className="w-full h-full object-cover"
