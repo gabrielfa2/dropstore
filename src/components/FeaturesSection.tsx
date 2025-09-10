953c813a82fadfdbc2340d9d84f5d143e69f2eaa
Gabriel <eugabrielfa07@gmail.com>
2025-09-10 18:10:15 -0300

    Updated FeaturesSection.tsx

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -101,7 +101,7 @@ const FeaturesSection = () => {
                 </div>
                 
                 <div>
-                  <h4 className="text-lg font-black text-orange-100 mb-1 text-outline-white">
+                  <h4 className="text-lg font-black text-orange-990 mb-1 text-outline-white">
                     {feature.title}
                   </h4>
                   <p className="text-white text-sm text-outline-white">
